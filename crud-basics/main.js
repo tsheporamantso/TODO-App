38255728916a6865e70339ce2842e2fc6521e0b2
tsheporamantso <tshepo.ramantso@outlook.com>
2023-09-13 18:17:26 +0200

    Add display post function to display data dynamically

--- crud-basics/main.js
+++ crud-basics/main.js
@@ -1,6 +1,7 @@
 const form = document.getElementById("form");
 const input = document.getElementById("input");
 const msg = document.getElementById("msg");
+const posts = document.getElementById("posts")
 
 form.addEventListener("submit", (e) => {
     e.preventDefault();
@@ -20,5 +21,19 @@ const data = {};
 
 const acceptData = () => {
     data["text"] = input.value
-    console.log(data);
+    displayPost();
+
+}
+
+const displayPost = () => {
+    posts.innerHTML += `
+    <div>
+    <p>${data.text}</p>
+    <span class="options">
+      <i class="fa-solid fa-pen-to-square fa-beat"></i>
+      <i class="fa-regular fa-trash-can fa-beat"></i>
+    </span>
+  </div>
+    `;
+    input.value = "";
 }
