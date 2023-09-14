41f521c1eb3de724e82ae9190fa2246264ad1391
tsheporamantso <tshepo.ramantso@outlook.com>
2023-09-14 13:47:41 +0200

    Add function to display date dynamically on the app

--- todo-app/main.js
+++ todo-app/main.js
@@ -3,6 +3,7 @@ const textInput = document.getElementById("textInput");
 const dateInput = document.getElementById("dateInput");
 const textarea = document.getElementById("textarea")
 const msg = document.getElementById("msg");
+const tasks = document.getElementById("tasks")
 
 form.addEventListener("submit",(e)=> {
     e.preventDefault();
@@ -23,7 +24,32 @@ const data = {};
 const acceptData = () => {
     data["text"] = textInput.value
     data["date"] = dateInput.value
-    data["discription"] = textarea.value
+    data["description"] = textarea.value
+    displayPost();
     console.log(data)
 }
 
+const displayPost = () => {
+    tasks.innerHTML += `
+    <div>
+    <span class="fw-bold">${data.text}</span>
+    <span class="small text-secondary">${data.date}</span>
+    <p>${data.description}</p>
+    <span class="options">
+      <i class="fa-solid fa-pen-to-square fa-beat-fade"></i>
+      <i onClick="deletePost(this)" class="fa-solid fa-trash-can fa-beat-fade"></i>
+    </span>
+  </div>  
+    `;
+    formReset();
+}
+
+const formReset = () => {
+    textInput.value = "";
+    dateInput.value = "";
+    textarea.value = "";
+}
+
+const deletePost = (e) => {
+    e.remove()
+}
