50faf428f54f17490772532e5e8a5b38e2949a21
tsheporamantso <tshepo.ramantso@outlook.com>
2023-09-14 13:12:43 +0200

    Add function to accept and store data in empty object

--- todo-app/main.js
+++ todo-app/main.js
@@ -1,6 +1,29 @@
-let form = document.getElementById("form");
+const form = document.getElementById("form");
+const textInput = document.getElementById("textInput");
+const dateInput = document.getElementById("dateInput");
+const textarea = document.getElementById("textarea")
+const msg = document.getElementById("msg");
 
 form.addEventListener("submit",(e)=> {
     e.preventDefault();
-    console.log("butoon clicked");
-})
+    formValidation();
+});
+
+const formValidation = () => {
+    if(textInput.value === ""){
+        msg.innerHTML = "Task cannot be blank!"
+    } else {
+        msg.innerHTML = ""
+        acceptData();
+    }
+}
+
+const data = {};
+
+const acceptData = () => {
+    data["text"] = textInput.value
+    data["date"] = dateInput.value
+    data["discription"] = textarea.value
+    console.log(data)
+}
+
